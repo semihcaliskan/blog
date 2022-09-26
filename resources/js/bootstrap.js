15fee07829e0810e25e386c4cbd147e0fc1dada0
Semih Caliskan <mail@semihcaliskan.com>
2022-09-26 14:33:19 +0300

    Install Larevel/ui

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,12 @@
-import _ from 'lodash';
-window._ = _;
+import loadash from 'lodash'
+window._ = loadash
+
+
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap'
+
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -7,8 +14,8 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+import axios from 'axios'
+window.axios = axios
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
