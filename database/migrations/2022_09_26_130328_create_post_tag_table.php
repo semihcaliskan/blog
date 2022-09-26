13f2dd9e5da0c5dcf01e7f71bece09383ea1f103
Semih Caliskan <mail@semihcaliskan.com>
2022-09-26 16:12:32 +0300

    Create base models and relations

---
+++ database/migrations/2022_09_26_130328_create_post_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignIdFor(Post::class);
+            $table->foreignIdFor(Tag::class);
+            $table->unique(['post_id','tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**
+ * Basit bir blog sistemi istiyoruz
+ * İçinde blog yazıları (Post) bulunacak
+ * Bu yazılar bir kullanıcıya (User) ait olacak
+ * Bu yazılar bir kategoriye (Category) bağlı olacak
+ * Bu yazılar birçok etikete (Tag) sahip olabilecek
+ *
+ * Ana sayfada en sonuncudan en eskiye, sayfa başı 5 tane gözükecek şekilde listeleme olacak
+ * Bir kullanıcının adına basılarak, o kullanıcın yazılarının listelemesi olacak
+ * Kategoriye göre listeleme olacak
+ * Etikete göre listeleme olacak
+ * Aktif bir kullanıcı varsa;
+ * Kullanıcı yeni bir yazı ekleyebilecek
+ * Kendi yazılarını düzenleyebilecek ve silebilecek
+ */
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
